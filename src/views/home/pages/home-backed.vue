04b0cd4efc6c3c0bbce720ad61b708c403cde60e
George Mo <george.mo@nbai.io>
2024-08-15 17:02:56 -0400

    crm backed using api

--- src/views/home/pages/home-backed.vue
+++ src/views/home/pages/home-backed.vue
@@ -1,59 +1,81 @@
 <template>
   <div class="carousel-container page-body w-100 pt-48 pb-16">
     <div class="card-title linear-title font-24 font-bold text-center mb-32">Backed By</div>
-    <el-row class="row-bg" :gutter="32" justify="center">
-      <template v-for="list in slideList.slice(0, 4)" :key="list">
-        <el-col :xs="12" :sm="6" :md="6" :lg="6" class="mb-32 flex">
-          <img :src="list.img" class="funders-img" />
-        </el-col>
-      </template>
-    </el-row>
-    <el-row class="row-bg" :gutter="32" justify="center">
-      <template v-for="list in slideList.slice(4)" :key="list">
-        <el-col :xs="12" :sm="6" :md="6" :lg="6" class="mb-32 flex">
-          <img :src="list.img" class="funders-img" />
-        </el-col>
-      </template>
-    </el-row>
+    <div v-for="(row, rowIndex) in chunkedPoweredImagesList" :key="rowIndex" class="funded-list flex flex-ai-center flex-jc-center mb-32">
+      <img 
+        v-for="(item, index) in row" 
+        :key="index" 
+        :src="item.url" 
+        class="funders-img" 
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import powerPage01 from '@/assets/img/Backed/Backed-01.png'
-import powerPage02 from '@/assets/img/Backed/Backed-02.png'
-import powerPage03 from '@/assets/img/Backed/Backed-03.png'
-import powerPage04 from '@/assets/img/Backed/Backed-04.png'
-import powerPage05 from '@/assets/img/Backed/Backed-05.png'
-import powerPage06 from '@/assets/img/Backed/Backed-06.png'
-import powerPage07 from '@/assets/img/Backed/Backed-07.png'
-import powerPage08 from '@/assets/img/Backed/Backed-08.png'
+// import powerPage01 from '@/assets/img/Backed/Backed-01.png'
+// import powerPage02 from '@/assets/img/Backed/Backed-02.png'
+// import powerPage03 from '@/assets/img/Backed/Backed-03.png'
+// import powerPage04 from '@/assets/img/Backed/Backed-04.png'
+// import powerPage05 from '@/assets/img/Backed/Backed-05.png'
+// import powerPage06 from '@/assets/img/Backed/Backed-06.png'
+// import powerPage07 from '@/assets/img/Backed/Backed-07.png'
+// import powerPage08 from '@/assets/img/Backed/Backed-08.png'
 
-const slideList = ref([
-  {
-    img: powerPage01
-  },
-  {
-    img: powerPage02
-  },
-  {
-    img: powerPage03
-  },
-  {
-    img: powerPage04
-  },
-  {
-    img: powerPage05
-  },
-  {
-    img: powerPage06
-  },
-  {
-    img: powerPage07
-  },
-  {
-    img: powerPage08
+// const slideList = ref([
+//   {
+//     img: powerPage01
+//   },
+//   {
+//     img: powerPage02
+//   },
+//   {
+//     img: powerPage03
+//   },
+//   {
+//     img: powerPage04
+//   },
+//   {
+//     img: powerPage05
+//   },
+//   {
+//     img: powerPage06
+//   },
+//   {
+//     img: powerPage07
+//   },
+//   {
+//     img: powerPage08
+//   }
+// ])
+
+import { ref, onMounted } from 'vue';
+
+interface PoweredImage {
+  url: string;
+}
+
+const poweredImagesList = ref<PoweredImage[]>([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch(import.meta.env.VITE_BASEAPI_PROXIMA + 'crm_backed/list'); 
+    const data = await response.json();
+
+    poweredImagesList.value = data.data;
+    console.log(poweredImagesList.value)
+  } catch (error) {
+    console.error('Error fetching images:', error);
   }
-])
+});
+
+const chunkedPoweredImagesList = computed(() => {
+  const chunks = [];
+  for (let i = 0; i < poweredImagesList.value.length; i += 4) {
+    chunks.push(poweredImagesList.value.slice(i, i + 4));
+  }
+  return chunks;
+});
 </script>
 
 <style lang="less" scoped>
